e6e97c65f98991c29f6c486e8e665b74bd1b5172
Scato Eggen <scato.eggen@gmail.com>
2013-08-18 10:48:20 +0200

    Moved tests for frame and tween to Timeline

--- test/animation/timeline.spec.js
+++ test/animation/timeline.spec.js
@@ -20,4 +20,54 @@ describe("Timeline", function () {
             expect(Timeline.ref('prop').is(map)).toBe(true);
         });
     });
+
+    describe("frame", function () {
+        it("adds a behavior for each of the frame properties", function () {
+            var timeline = Timeline.create();
+
+            timeline.frame({
+                'foo': 'bar'
+            });
+
+            var foo = timeline.prop('foo');
+
+            expect(foo(0)).toBe('bar');
+        });
+
+        it("transforms the behaviors for each of the frame properties", function () {
+        });
+
+        it("returns the timeline", function () {
+            var timeline = Timeline.create();
+
+            var result = timeline.frame({
+                'foo': 'bar'
+            });
+
+            expect(result).toBe(timeline);
+        });
+    });
+
+    describe("tween", function () {
+        it("applies the shape to two frames", function () {
+
+        });
+
+        it("adds its duration to the total duration", function () {
+            var timeline = Timeline.create()
+                .frame({'x': 10})
+                .tween(function () {}, 0.5);
+
+            expect(timeline.duration()).toBe(0.5);
+        });
+
+        it("returns the timeline", function () {
+            var timeline = Timeline.create()
+                .frame({'x': 10});
+
+            var result = timeline.tween(function () {}, 0.5);
+
+            expect(result).toBe(timeline);
+        });
+    });
 });

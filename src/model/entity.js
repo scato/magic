eff3b4baea408d87bee7dde712bf3cb2a1be1952
Scato Eggen <seggen@ibuildings.nl>
2013-08-31 22:33:39 +0200

    Fixups for Entity associations

--- src/model/entity.js
+++ src/model/entity.js
@@ -3,6 +3,38 @@
 var Root = require('../root');
 var Json = require('./json');
 
+function isArray(value) {
+	return Object.prototype.toString.call(value) === '[object Array]';
+}
+
+function add(array, value) {
+	array.push(value);
+}
+
+function remove(array, value) {
+	var index = array.indexOf(value);
+	
+	if (index !== -1) {
+		array.splice(index, 1);
+	}
+}
+
+function reset(object, key, value) {
+	if (isArray(object[key]())) {
+		remove(object[key](), value);
+	} else {
+		object[key](null);
+	}
+}
+
+function fixup(object, key, value) {
+	if (isArray(object[key]())) {
+		add(object[key](), value);
+	} else {
+		object[key](value);
+	}
+}
+
 module.exports = Json(Root.create()).
     override('field', function (base) {
         return function (name, init) {
@@ -10,5 +42,54 @@ module.exports = Json(Root.create()).
             
             return base(name, init);
         };
+    }).
+    def('hasOne', function (type, name, inverse) {
+    	return this.lazy(name, function () {
+    		var value = null;
+    		
+    		return function () {
+    			if (arguments.length === 0) {
+    				return value;
+    			} else {
+    				var prev = value;
+    				var next = arguments[0];
+    				
+    				if (next !== null && !next.is(type)) {
+    					throw new Error('Type mismatch when assigning value to \'' + name + '\'');
+    				}
+    				
+    				if (value === next) {
+    					return this;
+    				}
+    				
+    				value = next;
+    				
+    				if (inverse !== undefined && prev !== null) {
+    					reset(prev, inverse, this);
+    				}
+    				
+    				if (inverse !== undefined && next !== null) {
+    					fixup(next, inverse, this);
+    				}
+    				
+    				return this;
+    			}
+    		};
+    	});
+    }).
+    def('hasMany', function (type, name, inverse) {
+    	return this.lazy(name, function () {
+    		var elements = [];
+    		
+    		return function () {
+    			if (arguments.length === 0) {
+    				return elements;
+    			} else {
+    				elements = arguments[0];
+    				
+    				return this;
+    			}
+    		};
+    	});
     });
 
---
+++ test/model/entity.hasone.spec.js
@@ -0,0 +1,88 @@
+"use strict";
+
+var Entity = require('../../src/model/entity'),
+    field = require('../../src/magic').field;
+
+describe("Entity", function () {
+	var Location = Entity.create();
+	var Character = Entity.create();
+	var Item = Entity.create();
+	var Player = Entity.create();
+	
+	Player.
+		hasOne(Character, 'character', 'player');
+	
+	Character.
+		hasOne(Player, 'player', 'character').
+		hasOne(Location, 'location').
+		hasMany(Item, 'items', 'bearer');
+	
+	Item.
+		hasOne(Character, 'bearer', 'items');
+		
+    describe("hasOne", function () {
+    	it("has a default value of null", function () {
+    		expect(Character.location()).toBe(null);
+    	});
+    	
+    	it("rejects the wrong type", function () {
+    		expect(function () {
+    			var Frodo = Character.create();
+    			var Sting = Item.create();
+    			
+    			Frodo.location(Sting);
+    		}).toThrow(new Error(
+    			"Type mismatch when assigning value to 'location'"
+    		));
+    	});
+    	
+    	it("fixes up a one-to-one association", function () {
+    		var player = Player.create();
+    		var Gandalf = Character.create();
+    		
+    		player.character(Gandalf);
+    		expect(Gandalf.player()).toBe(player);
+    		
+    		player.character(null);
+    		expect(Gandalf.player()).toBe(null);
+    	});
+    	
+    	it("resets the previous one-to-one association", function () {
+    		var player1 = Player.create();
+    		var player2 = Player.create();
+    		var Gandalf = Character.create();
+    		
+    		player1.character(Gandalf);
+    		expect(player1.character()).toBe(Gandalf);
+    		
+    		player2.character(Gandalf);
+    		expect(player1.character()).toBe(null);
+    	});
+    	
+    	it("fixes up a many-to-one association", function () {
+    		var Sting = Item.create();
+    		var MithrilCoat = Item.create();
+    		var Frodo = Character.create();
+    		
+    		Sting.bearer(Frodo);
+    		MithrilCoat.bearer(Frodo);
+    		
+    		expect(Frodo.items()).toEqual([Sting, MithrilCoat]);
+    	});
+    	
+    	it("updates the previous many-to-one association", function () {
+    		var Sting = Item.create();
+    		var MithrilCoat = Item.create();
+    		var Frodo = Character.create();
+    		var Sam = Character.create();
+    		
+    		Sting.bearer(Frodo);
+    		MithrilCoat.bearer(Frodo);
+    		
+    		Sting.bearer(Sam);
+    		
+    		expect(Frodo.items()).toEqual([MithrilCoat]);
+    	});
+    });
+});
+
